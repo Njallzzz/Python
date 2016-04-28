55c16768e0e73fa67d2741256a53dbe8c4af1477
Njáll Hilmar Hilmarsson <gitlab@deadbyte.is>
2016-04-28 22:23:32 +0000

    hangman

--- Verkefni 2/hangman.py
+++ Verkefni 2/hangman.py
@@ -1,15 +1,20 @@
-from itertools import product as p
-from itertools import chain as c
+from itertools import product
+from itertools import chain
+from pprint import pprint as print
 
-def hangman(path, semi, guessed):
-    for d in p(['+', '-', ''], repeat=len(e)-1):
-        k = ''.join( str(v) for v in list(c.from_iterable(zip(e, d))) + [e[-1]] )
-        if eval(k) == t:
-            return k + "=" + str(t)
+def hangman(path, semifilled, guessed):
+    with open(path) as file:
+       data = file.read().splitlines()
 
+    data = [ x for x in set(data) if len(x) == len(semifilled) and x.isalpha() ]        # Remove all not of equal length and limits to english chars only
+    data = [ x for x in data if all(map(lambda n: semifilled[n[0]] == n[1] or semifilled[n[0]] == '-', enumerate(x))) ]
+    
+    data = [ x for x in data if not any(map(lambda n: semifilled[n[0]] == '-' and n[1] in guessed, enumerate(x))) ]
+    
+    print(data)
+    
 def _test(a,b,c,d):
     print( hangman(a,b,c) == d )
-
 
 def test():
     _test('all_words.txt', 's-a--o--s', 'aeiosu', ['scaffolds', 'shamrocks', 'standoffs'])
